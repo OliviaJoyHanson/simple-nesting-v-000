b5ef9c5602c20e315970218e41387d3d4d6591b0
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-15 14:50:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
-		 programmer_hash[:alan_kay][0] = {:know_for => "GUI"}
+		 programmer_hash[:alan_kay][0][0] = {:know_for => "GUI"}
 end
 
 def adding_to_dennis
